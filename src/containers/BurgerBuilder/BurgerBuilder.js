79e928ebc5510b5ba5cf7d8beeccd0d637ce1ce4
Giustino Borzacchiello <giustinob@gmail.com>
2019-10-03 22:43:21 +0200

    Create initial Burger app scaffolding

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <>
+                <div>Burger</div>
+                <div>Controls</div>
+            </>
+        );
+    }
+}
+
+export default BurgerBuilder;
