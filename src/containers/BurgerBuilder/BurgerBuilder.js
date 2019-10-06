6ff28e08aa97b2a0cda38fbec1b248863ac245de
Giustino Borzacchiello <giustinob@gmail.com>
2019-10-06 10:39:21 +0200

    Setup remove handler to remove ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -14,10 +14,10 @@ class BurgerBuilder extends Component {
 
     state = {
         ingredients: {
+            bacon: 0,
             salad: 0,
-            meat: 0,
             cheese: 0,
-            bacon: 0
+            meat: 0
         },
         totalPrice: 4
     }
@@ -40,15 +40,43 @@ class BurgerBuilder extends Component {
     }
 
     removeIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
 
+        if( oldCount > 0 ) {
+            const updatedCount = oldCount - 1;
+
+            const updatedIngredients = {
+                ...this.state.ingredients
+            };
+    
+            updatedIngredients[type] = updatedCount;
+    
+            const priceDeduction = INGREDIENT_PRICES[type];
+            const oldPrice = this.state.totalPrice;
+            const newPrice = oldPrice - priceDeduction;
+    
+            this.setState({ totalPrice: newPrice, ingredients: updatedIngredients});
+        }
+        
     }
 
     render() {
+
+        const disabledInfo = {
+            ...this.state.ingredients
+        }
+
+        for( let key in disabledInfo ) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
+
         return (
             <>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
-                    ingredientAdded={this.addIngredientHandler} />
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler} 
+                    disabled={disabledInfo} />
             </>
         );
     }
