f1e66aedc1bc339d92b5d6cbc1525c06ddeb7347
Giustino Borzacchiello <giustinob@gmail.com>
2019-10-06 07:03:34 +0200

    Add default message for empty state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,24 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
+
+    let ingredients = Object.keys(props.ingredients).map((ingredient) => {
+        return [...Array(props.ingredients[ingredient])].map((_, i) => {
+            
+            return <BurgerIngredient type={ingredient} key={`${ingredient}_${i}`} />;
+        });
+    }).reduce((arr, el) => {
+        return arr.concat(el);
+    }, []);
+
+    if( ingredients.length === 0 ) {
+        ingredients = <p>Please add some ingredients</p>;
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
-            <BurgerIngredient type='cheese' />
-            <BurgerIngredient type='meat' />
+            {ingredients}
             <BurgerIngredient type='bread-bottom' />
         </div>
     );
